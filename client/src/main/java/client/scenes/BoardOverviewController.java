fdde4dc35e53b0045fdbf7244cbb6272d40a6e3c
Andrei Stefan <A.Stefan-1@student.tudelft.nl>
2023-03-06 19:40:01 +0100

    Added List generation option, with default add button, and scroll wheel to see all lists

--- client/src/main/java/client/scenes/BoardOverviewController.java
+++ client/src/main/java/client/scenes/BoardOverviewController.java
@@ -1,4 +1,23 @@
 package client.scenes;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.HBox;
+
 public class BoardOverviewController {
+    public HBox hBox;
+    public TitledPane TODO;
+
+    //TODO make button inside titled pane top-center aligned
+    public void addList() {
+        Button tempButton = new Button("+");
+        TitledPane titledPane = new TitledPane("new list", tempButton);
+        titledPane.setPrefHeight(TODO.getPrefHeight());
+        titledPane.setMinWidth(TODO.getMinWidth());
+        titledPane.setAnimated(false);
+        titledPane.setContentDisplay(ContentDisplay.TOP);
+        titledPane.getContent().setStyle("-fx-alignment: top-center;");
+        hBox.getChildren().add(titledPane);
+    }
 }
