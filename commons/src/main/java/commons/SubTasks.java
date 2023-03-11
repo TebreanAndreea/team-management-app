1f06e0525d085207ec7782710873461af5334b0f
Cristian Flaviu Toadere <c.f.toadere@student.tudelft.nl>
2023-03-11 17:19:35 +0100

    Edited classes and made relations between tables

--- commons/src/main/java/commons/SubTasks.java
+++ commons/src/main/java/commons/SubTasks.java
@@ -1,12 +1,37 @@
 package commons;
 
+import javax.persistence.*;
+import java.util.Objects;
 
-
+@Entity
 public class SubTasks {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long stId;
     private String title;
     private boolean done;
+    @ManyToOne
+    @JoinColumn(name = "card_id")
+    private Card card;
+
+    /**
+     * Constructor for a SubTask object.
+     * @param title - title of the subtask
+     * @param card - card this subtask is assigned to
+     */
+    public SubTasks(String title, Card card) {
+        this.title = title;
+        this.card = card;
+        this.done = false;
+    }
+
+    /**
+     * Default constructor for a SubTask object.
+     */
+    @SuppressWarnings("unused")
+    public SubTasks(){
+
+    }
 
     /**
      * Getter for the id.
@@ -26,7 +51,7 @@ public class SubTasks {
 
     /**
      * Setter for the title.
-     * @param title the new title
+     * @param title - the new title
      */
     public void setTitle(String title) {
         this.title = title;
@@ -42,11 +67,47 @@ public class SubTasks {
 
     /**
      * Setter for the done boolean.
-     * @param done the new value of the boolean
+     * @param done - the new value of the boolean
      */
     public void setDone(boolean done) {
         this.done = done;
     }
 
+    /**
+     * Getter for the card parent.
+     * @return - this subtask's card parent
+     */
+    public Card getCard() {
+        return card;
+    }
 
+    /**
+     * Setter for the parent card.
+     * @param card - the new parent card of the subtask
+     */
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    /**
+     * Equals method for comparing two subtasks.
+     * @param o - the subtask compared to this object
+     * @return - true if the subtasks are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SubTasks)) return false;
+        SubTasks subTasks = (SubTasks) o;
+        return stId == subTasks.stId && done == subTasks.done && title.equals(subTasks.title) && card.equals(subTasks.card);
+    }
+
+    /**
+     * Hashcode method for the subtask.
+     * @return - this subtask's hashcode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(stId, title, done, card);
+    }
 }
--- commons/src/main/java/commons/Tags.java
+++ commons/src/main/java/commons/Tags.java
@@ -1,31 +1,37 @@
 package commons;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Tags {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long tagId;
     private String title;
     @ManyToMany
-    private ArrayList<Card> cards;
+    @JoinTable(
+        name = "tagged_cards",
+        joinColumns = @JoinColumn(name = "card_id"),
+        inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private List<Card> cards;
 
     /**
      * Constructor for the Tags class.
      * @param title - the title to the class
-     * @param cards - the cards which have the tag in them
      */
-    public Tags(String title, ArrayList<Card> cards) {
+    public Tags(String title) {
         this.title = title;
-        this.cards = cards;
+        this.cards = new ArrayList<>();
     }
 
     /**
      * Default constructor.
      */
+    @SuppressWarnings("unused")
     public Tags() {
 
     }
@@ -48,7 +54,7 @@ public class Tags {
 
     /**
      * Setter for the title of the tag.
-     * @param title
+     * @param title - title of the tag item
      */
     public void setTitle(String title) {
         this.title = title;
@@ -58,7 +64,35 @@ public class Tags {
      * Getter for the cards.
      * @return the list of cards
      */
-    public ArrayList<Card> getCards() {
+    public List<Card> getCards() {
         return cards;
+    }
+
+    /**
+     * Setter for list of cards attributed to this tag.
+     * @param cards - Card items that have this tag assigned
+     */
+    public void setCards(List<Card> cards){this.cards = cards;}
+
+    /**
+     * Equals method for comparing two tags.
+     * @param o - the tag compared to this object
+     * @return - true if the tags are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tags)) return false;
+        Tags tags = (Tags) o;
+        return tagId == tags.tagId && title.equals(tags.title) && cards.equals(tags.cards);
+    }
+
+    /**
+     * Hashcode method for the tag.
+     * @return - this tag's hashcode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(tagId, title, cards);
     }
 }
