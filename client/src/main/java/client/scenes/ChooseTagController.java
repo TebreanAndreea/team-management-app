7bdd82236d50ad0faa6b37f6e10fe384a4a1f087
Vassil Guenov <V.V.Guenov@student.tudelft.nl>
2023-04-08 16:38:16 +0200

    Corrected a tagging a card and drag & drop synchro issue

--- client/src/main/java/client/scenes/ChooseTagController.java
+++ client/src/main/java/client/scenes/ChooseTagController.java
@@ -8,6 +8,7 @@ import commons.Board;
 import commons.Card;
 import commons.Listing;
 import commons.Tag;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -20,7 +21,6 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import javax.inject.Inject;
-
 import java.io.IOException;
 import java.util.List;
 
@@ -36,6 +36,7 @@ public class ChooseTagController {
     private ServerUtils server;
     public Listing list;
     private long cardId;
+    private Card curCard;
     private Board board = new Board("test", "", "");
     private String fileName = "user_files/temp.txt";
 
@@ -56,6 +57,7 @@ public class ChooseTagController {
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
+
     /**
      * Setter for the list.
      *
@@ -64,6 +66,7 @@ public class ChooseTagController {
     public void setList(Listing list) {
         this.list = list;
     }
+
     /**
      * Setter for the current card.
      *
@@ -71,6 +74,26 @@ public class ChooseTagController {
      */
     public void setCardId(long cardId) {
         this.cardId = cardId;
+        this.curCard = server.getCardsById(cardId);
+    }
+
+    public void initialize() {
+        server.registerForUpdatesCard(q -> Platform.runLater(() ->
+        {
+            board = server.getBoardByID(board.getBoardId());
+            List<Listing> lists = board.getLists();
+            for (Listing l : lists) {
+                for (Card c : l.getCards()) {
+                    if (c.getName().equals(curCard.getName())) {
+                        setList(l);
+                        setCardId(c.getCardId());
+                    }
+                }
+            }
+            refresh();
+        }));
+
+
     }
 
     /**
@@ -86,9 +109,8 @@ public class ChooseTagController {
 
     /**
      * Empty controler for class.
-     *
      */
-    public ChooseTagController(){
+    public ChooseTagController() {
 
     }
 
@@ -113,20 +135,20 @@ public class ChooseTagController {
 
     /**
      * Refreshing the available tags.
-     *
      */
     public void refresh() {
+        vbox.getChildren().clear();
         List<Tag> tags = board.getTags();
         Card card = server.getCardsById(cardId);
-        for(Tag tag: tags) {
+        for (Tag tag : tags) {
             CheckBox checkBox = new CheckBox(tag.getTitle());
-           // checkBox.setStyle("-fx-font-size: 40px;");
-            if(tag.getColor() != null) {
+            // checkBox.setStyle("-fx-font-size: 40px;");
+            if (tag.getColor() != null) {
                 Color color = Color.web(tag.getColor());
                 Background background = new Background(new BackgroundFill(color, null, null));
                 checkBox.setBackground(background);
             }
-            checkBox.setMinSize(200,50);
+            checkBox.setMinSize(200, 50);
             checkBox.setAlignment(Pos.CENTER);
             vbox.setAlignment(Pos.CENTER);
             vbox.setSpacing(20);
@@ -135,15 +157,14 @@ public class ChooseTagController {
             checkBox.setSelected(card.getTags().contains(tag));
             checkBox.selectedProperty().addListener((obs, old, newVal) -> {
                 server.sendList(list);
-                if(newVal == true) {
+                if (newVal == true) {
                     server.addTag(card, tag);
-                  //  tag.getCards().add(card);
-                }
-                else {
+                    //  tag.getCards().add(card);
+                } else {
                     server.removeTag(card, tag);
-                  //  tag.getCards().remove(card);
+                    //  tag.getCards().remove(card);
                 }
-              //  server.sendList(list);
+                //  server.sendList(list);
                 //server.saveCard(card);
 
             });
